33f20cf7d586f758b79fd748128b5acddcc91e4b
codesyariah122 <pujiermanto@gmail.com>
2024-09-29 08:46:05 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  ssr: false,
+  experimental: {
+    payloadExtraction: false
+  },
+  router: {
+    options: {
+      strict: false
+    }
+  },
   css: ['~/assets/css/tailwind.css'],
   postcss: {
     plugins: {
